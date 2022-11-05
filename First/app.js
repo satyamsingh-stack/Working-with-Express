4f15aa99ed786f7bf9c723154718866c4d768a4b
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-11-05 18:19:40 +0530

    Create app.js

---
+++ First/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to my Home Page");
+})
+
+app.get('/about',(req,res)=>{
+    res.send("Welcome to about page");
+})
+
+app.get('/contact',(req,res)=>{
+    res.send("Welcome to Contact Page");
+})
+
+app.get('/temp',(req,res)=>{
+    res.send("Welcome to temp Page");
+})
+port = 4200;
+app.listen(4200,()=>{
+    console.log("Lestining the port",port);
+})
